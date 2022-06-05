bfb828f83e94f50bb84634cfc9fc8b987e5721fc
winya1 <50223740+winya1@users.noreply.github.com>
2022-06-05 19:47:58 -0400

    Create Solution.java
    DNA
    DNA, the carrier of genetic information in living things, has been used in criminal justice for decades. But how, exactly, does DNA profiling work? Given a sequence of DNA, how can forensic investigators identify to whom it belongs?
    
    Well, DNA is really just a sequence of molecules called nucleotides, arranged into a particular shape (a double helix). Each nucleotide of DNA contains one of four different bases: adenine (A), cytosine (C), guanine (G), or thymine (T). Every human cell has billions of these nucleotides arranged in sequence. Some portions of this sequence (i.e. genome) are the same, or at least very similar, across almost all humans, but other portions of the sequence have a higher genetic diversity and thus vary more across the population.
    
    One place where DNA tends to have high genetic diversity is in Short Tandem Repeats (STRs). An STR is a short sequence of DNA bases that tends to be repeated back-to-back numerous times at specific locations in DNA. The number of times any particular STR repeats varies a lot among different people. In the DNA samples below, for example, Alice has the STR AGAT repeated four times in her DNA, while Bob has the same STR repeated five times.
    
    Alice: CTAGATAGATAGATAGATGACTA.
    
    Bob: CTAGATAGATAGATAGATAGATT.
    
    Using multiple STRs, rather than just one, can improve the accuracy of DNA profiling. If the probability that two people have the same number of repeats for a single STR is 5%, and the analyst looks at 10 different STRs, then the probability that two DNA samples match purely by chance is about 1 in 1 quadrillion (assuming all STRs are independent of each other). So if two DNA samples match in the number of repeats for each of the STRs, the analyst can be pretty confident they came from the same person. CODIS, The FBI's DNA database, uses 20 different STRs as part of its DNA profiling process.
    
    Consider the following database, containing the information about 3 STRs for two people:
    3 AGAT AATG TATC
    2
    Alice 28 42 14
    Bob 17 22 19
    This database encodes that supported STRs are AGAT, AATG and TATC; and that Alice has the sequence AGAT repeated 28 times consecutively somewhere in her DNA, the sequence AATG repeated 42 times, and TATC repeated 14 times. Bob, meanwhile, has those same three STRs repeated 17 times, 22 times, and 19 times, respectively.
    So given a sequence of DNA, how might you identify to whom it belongs? Well, imagine that you looked through the DNA sequence for the longest consecutive sequence of repeated AGATs and found that the longest sequence was 17 repeats long. If you then found that the longest sequence of AATGs is 22 repeats long, and the longest sequence of TATC is 19 repeats long, that would provide pretty good evidence that the DNA was Bob's. Of course, it's also possible that once you take the counts for each of the STRs, it doesn't match anyone in your DNA database, in which case you have no match.
    
    Your task is to write a program that will take a DNA database with STR counts for a list of individuals, a list of DNAs, and then output to whom the DNAs (most likely) belong.
    
    Source: http://nifty.stanford.edu/2020/dna/.
    
    Input
    The first line of the input contains S: the number of STRs, followed by the STRs themselves. Each STR is a string containing capital latin letters A, C, G, or T.
    
    The next line contains N: the number of people in the database.
    
    The next N lines contain the information about every person: their name, and the number of times the corresponding STR is replicated in their DNA.
    
    The next line contains M: the number of samples to identify.
    
    The following M lines are test cases: strings containing capital latin letters A, C, G, or T.
    
    Output
    For each sample, output one line containing Case #x: y, where x is the sample number (starting from 1), and y is either the name of the person, to whom the sample belongs, or string No match if such person does not exist in the database.

---
+++ DNA/Solution.java
@@ -0,0 +1,272 @@
+import java.util.*;
+
+
+class KMP_String_Matching {
+    List<Integer> KMPSearch(String pat, String txt, int divider) {
+        int M = pat.length();
+        int N = txt.length();
+
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[] = new int[M];
+        int j = 0; // index for pat[]
+
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps);
+
+        List<Integer> indices = new ArrayList<Integer>();
+        List<Integer> counts = new ArrayList<Integer>();
+
+        int i = 0; // index for txt[]
+        int count = 1; // to count instances of consecutive patterns
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                int index = i - j;
+                j = lps[j - 1];
+                indices.add(index);
+            }
+
+            // mismatch after j matches
+            else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+
+        if (indices.size() == 1) {
+            counts.add(1); //only one match occurred
+        }
+
+        for (int g = 0; g < indices.size() - 1; g++) {
+            if (indices.get(g) + divider == indices.get(g + 1)) {
+                count = count + 1; //if the next pat is the same - increase count
+            }
+            if (indices.get(g) + divider == indices.get(indices.size() - 1)) {
+                counts.add(count);
+                count = 1; //if there are only 2 occurrences of the pat & that makes up the whole string
+                //to-do: check if I need this count? seems like I don't
+            }
+            if (indices.get(g) + divider != indices.get(g + 1)) {
+                counts.add(count);
+                count = 1; //if no pattern in-between, then this will add the occurences of the pat; reset count to 1
+            }
+
+        }
+        if (indices.size() > 1) {
+            if (indices.get(indices.size() - 2) + divider != indices.get(indices.size() - 1)) {
+                counts.add(1);
+            } //to account for the end pattern if it's not repeating
+        }
+
+        return counts;
+
+    }
+
+
+
+    int KMPSearch(String pat, String txt)
+    {
+        int M = pat.length();
+        int N = txt.length();
+
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[] = new int[M];
+        int j = 0; // index for pat[]
+
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat,M,lps);
+
+        int i = 0; // index for txt[]
+        int res = 0;
+        int next_i = 0;
+
+        while (i < N)
+        {
+            if (pat.charAt(j) == txt.charAt(i))
+            {
+                j++;
+                i++;
+            }
+            if (j == M)
+            {
+                // When we find pattern first time,
+                // we iterate again to check if there
+                // exists more pattern
+                j = lps[j-1];
+                res++;
+
+                // We start i to check for more than once
+                // appearance of pattern, we will reset i
+                // to previous start+1
+                if (lps[j]!=0)
+                    i = ++next_i;
+                j = 0;
+            }
+
+            // mismatch after j matches
+            else if (i < N && pat.charAt(j) != txt.charAt(i))
+            {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j-1];
+                else
+                    i = i+1;
+            }
+        }
+        return res;
+    }
+
+    void computeLPSArray(String pat, int M, int lps[]) {
+        // length of the previous longest prefix suffix
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+
+                    // Also, note that we do not increment
+                    // i here
+                } else // if (len == 0)
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+
+    }
+
+    class Solution {
+
+
+        public static void main(String[] args) {
+            HashMap<String, List<Integer>> map = new LinkedHashMap<>();
+            List<String> names = new ArrayList<String>();
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine().trim().replaceAll("  +", " ");
+            String[] STRs = input.split(" ");
+            int noOfSTRs = Integer.parseInt(STRs[0]);
+            int peopleDatabase = Integer.parseInt(in.nextLine());
+
+            //to add every person as a key-value pair in a hashmap
+            for (int i = 0; i < peopleDatabase; i++) {
+                String person = in.nextLine().trim().replaceAll("  +", " ");
+                String[] data = person.split(" ");
+                List<Integer> strData = new ArrayList<Integer>();
+                names.add(data[0]);
+                for (int j = 1; j <= noOfSTRs; j++) {
+                    strData.add(Integer.valueOf(data[j]));
+                }
+                map.put(data[0], strData);
+            }
+
+            int testCases = Integer.parseInt(in.nextLine());
+
+            List<String> who = null;
+            //for every input
+            for (int k = 0; k < testCases; k++) {
+                String DNA = in.nextLine();
+                int match = 0;
+                who = new ArrayList<>();
+
+                //with every input, parse out STRs and match them to people
+                for (int v = 1; v <= noOfSTRs; v++) {
+                    List<Integer> ans = new KMP_String_Matching().KMPSearch(STRs[v], DNA, STRs[v].length());
+                    int numMatch = new KMP_String_Matching().KMPSearch(STRs[v], DNA); //num match is only for 0s
+                    String name = "";
+                    for (String key : map.keySet()) {
+                        List<Integer> listPerName = map.get(key);
+                        int indexValue = listPerName.get(v - 1);
+                        if (numMatch == 0 &&  indexValue == 0) {
+                            match = match + 1;
+                            name = name + key;
+                         //accounts for 0 matches
+                        }
+
+                        if (ans.isEmpty() != true && Collections.max(ans) == indexValue) {
+                            match = match + 1;
+                            name = name + key;
+                            //if you find the max pat occurrence to match the person, helps avoid having duplicate matches
+                            //else if only occurs if the max doesn't come up
+                            //to-do might not be most efficient though considering max = O(n) might try to remove
+
+                        } else if (ans.contains(indexValue)) {
+                            match = match + 1;
+                            name = name + key;
+                        }
+                    }
+                    who.add(name);
+                }
+
+                int matched = 0; //matches w/ list of names created from matching STRs -
+                // if appear # of STRs times, then count this
+                int found = 0; //found is when there's match for every pat
+                int sum = 0; // used to sort the names
+                String nameMatch = "";
+                List<String> namesMatched = new ArrayList<>();
+                List<String> namesMatchedSorted = new ArrayList<>();
+
+
+                for (int z = 0 ; z < names.size(); z++) {
+                    for (String whoString : who) {
+
+                        int ans = new KMP_String_Matching().KMPSearch(names.get(z), whoString);
+                        if (ans > 0) {
+                            matched = matched + 1;
+                        }
+                        if (matched == noOfSTRs) {
+                            found = found + 1;
+                            namesMatched.add(names.get(z));
+                            nameMatch = nameMatch + names.get(z);
+                        }
+                    }
+                    matched = 0;
+                }
+
+                if (found == 1) {
+                    System.out.println("Case #" + (k + 1) + ": " + nameMatch);
+                }
+                if (found == 0) {
+                    System.out.println("Case #" + (k + 1) + ": " + "No match");
+                } else if (found > 1) {
+                     for (String name : namesMatched) {
+                         List<Integer> listPerMatchedName = map.get(name);
+                         int sumOfMatch = listPerMatchedName.stream().mapToInt(Integer::intValue).sum();
+                         if (sumOfMatch > sum) {
+                             sum = sumOfMatch;
+                             namesMatchedSorted.add(name);
+                         }
+                     }
+                     System.out.println("Case #" + (k + 1) + ": " + namesMatchedSorted.get(namesMatchedSorted.size() - 1));
+                 }
+                   }
+
+            }
+        }
+
+}
