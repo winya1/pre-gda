f3e41074e95a7c89a8cf751dc9d6dd406bf01f7e
winya1 <50223740+winya1@users.noreply.github.com>
2022-07-11 16:39:22 -0400

    Update Solution.java

--- Week04/DNA/Solution.java
+++ Week04/DNA/Solution.java
@@ -88,25 +88,17 @@ class KMP_String_Matching {
             }
             if (j == M)
             {
-                // When we find pattern first time,
-                // we iterate again to check if there
-                // exists more pattern
+          
                 j = lps[j-1];
                 res++;
 
-                // We start i to check for more than once
-                // appearance of pattern, we will reset i
-                // to previous start+1
                 if (lps[j]!=0)
                     i = ++next_i;
                 j = 0;
             }
 
-            // mismatch after j matches
             else if (i < N && pat.charAt(j) != txt.charAt(i))
             {
-                // Do not match lps[0..lps[j-1]] characters,
-                // they will match anyway
                 if (j != 0)
                     j = lps[j-1];
                 else
@@ -117,28 +109,23 @@ class KMP_String_Matching {
     }
 
     void computeLPSArray(String pat, int M, int lps[]) {
-        // length of the previous longest prefix suffix
+
         int len = 0;
         int i = 1;
-        lps[0] = 0; // lps[0] is always 0
-
-        // the loop calculates lps[i] for i = 1 to M-1
+        lps[0] = 0; 
+        
         while (i < M) {
             if (pat.charAt(i) == pat.charAt(len)) {
                 len++;
                 lps[i] = len;
                 i++;
-            } else // (pat[i] != pat[len])
+            } else 
             {
-                // This is tricky. Consider the example.
-                // AAACAAAA and i = 7. The idea is similar
-                // to search step.
                 if (len != 0) {
                     len = lps[len - 1];
 
-                    // Also, note that we do not increment
-                    // i here
-                } else // if (len == 0)
+                
+                } else 
                 {
                     lps[i] = len;
                     i++;
